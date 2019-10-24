7710f7f91c6232cb84c86ad23b5a35441a3f1ca9
Lucas-Zorzo <lucas.zorzo@ibm.com>
2019-10-24 10:50:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Fragment } from 'react';
 import './App.css';
 import PrimarySearchAppBar from './components/PrimarySearchAppBar';
-import TemporaryDrawer from './components/TemporaryDrawer'
 
 function App() {
   return (
     <Fragment>
       <PrimarySearchAppBar />
-      <TemporaryDrawer />
     </Fragment>
   );
 }
--- src/components/PrimarySearchAppBar.jsx
+++ src/components/PrimarySearchAppBar.jsx
@@ -14,6 +14,7 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import MailIcon from '@material-ui/icons/Mail';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
+import TemporaryDrawer from './TemporaryDrawer'
 
 const useStyles = makeStyles(theme => ({
     grow: {
@@ -187,6 +188,7 @@ export default function PrimarySearchAppBar() {
                             inputProps={{ 'aria-label': 'search' }}
                         />
                     </div>
+                    <TemporaryDrawer /> {/* look at this LoL */}
                     <div className={classes.grow} />
                     <div className={classes.sectionDesktop}>
                         <IconButton
